0246e562514ecb0afb20aed471da7fe704cbad88
cju-wkdal <junmo4484@gmail.com>
2024-03-24 18:48:23 +0900

    nan test 6

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 a,b = map(int, input().split())
 
-if a > 0 and b < 10:
+if a >= 0 and b <= 10000:
+        print(a+b)
+        print(a-b)
+        print(a*b)
         print(a/b)
+        print(a%b)
 else:
     print("a는 0보다 커야 하고, b는 10보다 작아야 합니다.")
